19690aa8c928c9a75921bc3020f8a82b04d108d2
blinbingo <ian.oddssports@gmail.com>
2025-04-10 20:29:53 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
-
+import styles from "../styles/style.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="body">
-      <div className="card">
-        <h1>Bem-vindo ao Bingo do Blindado</h1>
+    <div className={styles.body}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>Bem-vindo ao Bingo do Blindado</h1>
         <Link href="/simulador">
-          <button className="button">🎯 CRIAR SORTEIO</button>
+          <button className={styles.button}>🎯 Jogar</button>
         </Link>
       </div>
     </div>
   );
 }
-
