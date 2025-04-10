c35e54543abf60ef96f5d2602605203d6b716e18
blinbingo <ian.oddssports@gmail.com>
2025-04-10 20:27:10 -0300

    Update simulador.js

--- pages/simulador.js
+++ pages/simulador.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import * as XLSX from "xlsx";
-import "../styles/style.css";
+import styles from "../styles/style.module.css";
+
 
 const gerarCartela = (id) => {
   const numeros = new Set();
